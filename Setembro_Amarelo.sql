da862dd5d0ee10f863cc980a10e6f5bd54f72bad
ludmillaifrn <144167564+ludmillaifrn@users.noreply.github.com>
2025-10-08 12:46:46 -0300

    Update Setembro_Amarelo.sql
    novo

--- Setembro_Amarelo.sql
+++ Setembro_Amarelo.sql
@@ -1,8 +1,6 @@
-CREATE DATABASE  IF NOT EXISTS `setembroamarelo` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `setembroamarelo`;
 -- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
 --
--- Host: localhost    Database: setembroamarelo
+-- Host: localhost    Database: autopercepcao_db
 -- ------------------------------------------------------
 -- Server version	8.0.36
 
@@ -21,26 +19,22 @@ USE `setembroamarelo`;
 -- Table structure for table `respostasquestionario`
 --
 
-DROP TABLE IF EXISTS `respostasquestionario`;
+DROP TABLE IF EXISTS `RespostasQuestionario`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `respostasquestionario` (
+CREATE TABLE `RespostasQuestionario` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `usuario_id` int DEFAULT NULL,
-  `resposta_q1` int DEFAULT NULL,
-  `resposta_q2` int DEFAULT NULL,
-  `resposta_q3` int DEFAULT NULL,
-  `resposta_q4` int DEFAULT NULL,
-  `resposta_q5` int DEFAULT NULL,
-  `resposta_q6` int DEFAULT NULL,
-  `resposta_q7` int DEFAULT NULL,
-  `pontuacao_total` int DEFAULT NULL,
-  `pontuacao_percentual` decimal(5,2) DEFAULT NULL,
-  `nivel_bem_estar` varchar(20) DEFAULT NULL,
-  `data_submissao` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  KEY `usuario_id` (`usuario_id`),
-  CONSTRAINT `respostasquestionario_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`)
+  `usuario_id` varchar(255) NOT NULL,
+  `resposta_q1` int NOT NULL,
+  `resposta_q2` int NOT NULL,
+  `resposta_q3` int NOT NULL,
+  `resposta_q4` int NOT NULL,
+  `resposta_q5` int NOT NULL,
+  `resposta_q6` int NOT NULL,
+  `resposta_q7` int NOT NULL,
+  `pontuacao_total` int NOT NULL,
+  `nivel_bem_estar` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -48,35 +42,9 @@ CREATE TABLE `respostasquestionario` (
 -- Dumping data for table `respostasquestionario`
 --
 
-LOCK TABLES `respostasquestionario` WRITE;
+LOCK TABLES `RespostasQuestionario` WRITE;
 /*!40000 ALTER TABLE `respostasquestionario` DISABLE KEYS */;
 /*!40000 ALTER TABLE `respostasquestionario` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `usuarios`
---
-
-DROP TABLE IF EXISTS `usuarios`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `usuarios` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nome` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `senha` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `usuarios`
---
-
-LOCK TABLES `usuarios` WRITE;
-/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -88,4 +56,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-10-07 11:31:13
+-- Dump completed on 2025-10-08 11:06:11
